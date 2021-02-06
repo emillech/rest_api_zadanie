88c35904a3f2763272a1d83e74cb1d15e4994ab1
emil <contact@emillech.com.pl>
2021-02-06 12:54:59 +0100

    app and db configuration

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zadanie_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
